96b4290b709ec678ed1e468e9a455e64c4ae7e29
NITHIN_KANDI <116893619+NITHIN-KANDI@users.noreply.github.com>
2024-03-12 11:31:22 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,19 +1,19 @@
 def categorical_to_numerical(categories):
     # Dictionary to store the encoding of each category
-    encoding_val = {}
+    encoding_dict = {}
     # Counter to keep track of the numerical encoding
-    count = 0
+    numerical_value = 0
     
     # Iterate through each category
-    for i in categories:
+    for category in categories:
         # Check if the category is not already encoded
-        if i not in encoding_val:
+        if category not in encoding_dict:
             # Assign a numerical value to the category and increment the counter
-            encoding_val[i] = count
-            count += 1
+            encoding_dict[category] = numerical_value
+            numerical_value += 1
     
     # Return a list of numerical values corresponding to the input categories
-    return [encoding_val[i] for i in categories]
+    return [encoding_dict[category] for category in categories]
 
 # Example categories
 categories = ['One', 'Two', 'One', 'Three', 'Three', 'One', 'Two']
